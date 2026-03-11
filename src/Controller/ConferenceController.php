e8ddf4b7fdfb349feea9e0f2a38750033b79a0b1
Adrien Roches <adrien@roc-it.tech>
2026-03-11 10:29:42 +0100

    [sf-start] Generate ConferenceController

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Conference;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ConferenceController extends AbstractController
+{
+    #[Route('/conference/{name}/{start}/{end}', name: 'app_conference_new')]
+    public function newConference(string $name, string $start, string $end, /* ... */): Response
+    {
+        $conference = (new Conference())
+            ->setName($name)
+            ->setDescription('Some generic description')
+            ->setAccessible(true)
+            ->setStartAt(new \DateTimeImmutable($start))
+            ->setEndAt(new \DateTimeImmutable($end))
+        ;
+
+        //...
+
+        return new Response('Conference created');
+    }
+}
