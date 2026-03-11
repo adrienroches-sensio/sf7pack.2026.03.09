a1727eccdcb1d6bb878acd2dd359f738a82c832b
Adrien Roches <adrien@roc-it.tech>
2026-03-11 11:38:00 +0100

    [sf-start] Add search query in repository

--- src/Repository/ConferenceRepository.php
+++ src/Repository/ConferenceRepository.php
@@ -3,8 +3,10 @@
 namespace App\Repository;
 
 use App\Entity\Conference;
+use DateTimeImmutable;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use InvalidArgumentException;
 
 /**
  * @extends ServiceEntityRepository<Conference>
@@ -14,6 +16,36 @@ class ConferenceRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Conference::class);
+    }
+
+    /**
+     * @return list<Conference>
+     *
+     * @throws InvalidArgumentException When both $start and $end are null. (At least one must be provided)
+     */
+    public function searchBetweenDates(DateTimeImmutable|null $start, DateTimeImmutable|null $end): array
+    {
+        if (null === $start && null === $end) {
+            throw new InvalidArgumentException('At least one date must be provided');
+        }
+
+        $qb = $this->createQueryBuilder('conference');
+
+        if (null !== $start) {
+            $qb
+                ->andWhere($qb->expr()->gte('conference.startAt', ':start'))
+                ->setParameter('start', $start)
+            ;
+        }
+
+        if (null !== $end) {
+            $qb
+                ->andWhere('conference.endAt <= :end')
+                ->setParameter('end', $end)
+            ;
+        }
+
+        return $qb->getQuery()->getResult();
     }
 
 //    /**
