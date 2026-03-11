9046d8c1f54dadf3f4364665c9a83f3df80c1f91
Adrien Roches <adrien@roc-it.tech>
2026-03-11 14:49:34 +0100

    [sf-start] Use the ConferenceType in the newConference controller

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,8 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Conference;
+use App\Form\ConferenceType;
 use App\Repository\ConferenceRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,29 +13,19 @@ use Symfony\Component\Routing\Requirement\Requirement;
 class ConferenceController extends AbstractController
 {
     #[Route(
-        path: '/conference/{name}/{start}/{end}',
+        path: '/conference/new',
         name: 'app_conference_new',
-        requirements: [
-            'name' => Requirement::ASCII_SLUG,
-            'start' => Requirement::DATE_YMD,
-            'end' => Requirement::DATE_YMD,
-        ],
         methods: ['GET'],
     )]
-    public function newConference(string $name, string $start, string $end, EntityManagerInterface $entityManager): Response
+    public function newConference(): Response
     {
-        $conference = (new Conference())
-            ->setName($name)
-            ->setDescription('Some generic description')
-            ->setAccessible(true)
-            ->setStartAt(new \DateTimeImmutable($start))
-            ->setEndAt(new \DateTimeImmutable($end))
-        ;
+        $conference = new Conference();
 
-        $entityManager->persist($conference);
-        $entityManager->flush();
+        $form = $this->createForm(ConferenceType::class, $conference);
 
-        return new Response('Conference created');
+        return $this->render('conference/new.html.twig', [
+            'form' => $form,
+        ]);
     }
 
     #[Route(
