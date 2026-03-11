451ed060ebb0d573712aaf2ecbe719e2e32be90f
Adrien Roches <adrien@roc-it.tech>
2026-03-11 13:55:47 +0100

    [sf-start] Update fixtures to have almost all usecases

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Conference;
 use App\Entity\Organization;
+use App\Entity\Volunteering;
 use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -27,6 +28,41 @@ class AppFixtures extends Fixture
             $manager->persist($symfonyLive);
         }
 
+        $symfony = $this->createOrganization('Symfony SAS');
+        $manager->persist($symfony);
+
+        $symfonyCon2025 = $this->createConference(
+            name: 'SymfonyCon 2025 (multiple organizations)',
+            start: new DateTimeImmutable('2025-11-27'),
+            organizations: [$symfony, $sensioLabs],
+        );
+        $manager->persist($symfonyCon2025);
+
+        $conferenceWithoutOrganizations = $this->createConference(
+            name: 'Conference without organizations',
+            start: new DateTimeImmutable('2020-02-12'),
+            accessible: false,
+            prerequisites: 'Some prerequisites',
+        );
+        $manager->persist($conferenceWithoutOrganizations);
+
+        $conferenceWithVolunteersStart = new DateTimeImmutable('2026-02-12');
+        $volunteer1 = $this->createVolunteer($conferenceWithVolunteersStart);
+        $volunteer2 = $this->createVolunteer($conferenceWithVolunteersStart);
+        $volunteer3 = $this->createVolunteer($conferenceWithVolunteersStart);
+        $volunteer4 = $this->createVolunteer($conferenceWithVolunteersStart);
+        $volunteer5 = $this->createVolunteer($conferenceWithVolunteersStart);
+        $volunteers = [$volunteer1, $volunteer2, $volunteer3, $volunteer4, $volunteer5];
+
+        array_map($manager->persist(...), $volunteers);
+
+        $conferenceWithVolunteers = $this->createConference(
+            name: 'Conference with volunteers',
+            start: new DateTimeImmutable('2026-02-12'),
+            volunteers: $volunteers,
+        );
+        $manager->persist($conferenceWithVolunteers);
+
         $manager->flush();
     }
 
@@ -47,6 +83,7 @@ class AppFixtures extends Fixture
         bool $accessible = true,
         string|null $description = null,
         string|null $prerequisites = null,
+        array $volunteers = [],
     ): Conference {
         $conference = new Conference();
         $conference->setName($name);
@@ -60,6 +97,21 @@ class AppFixtures extends Fixture
             $conference->addOrganization($organization);
         }
 
+        foreach ($volunteers as $volunteering) {
+            $conference->addVolunteering($volunteering);
+        }
+
         return $conference;
+    }
+
+    private function createVolunteer(
+        DateTimeImmutable $startAt,
+        DateTimeImmutable|null $endAt = null,
+    ): Volunteering {
+        $volunteering = new Volunteering();
+        $volunteering->setStartAt($startAt);
+        $volunteering->setEndAt($endAt ?? $startAt->modify('+1 day'));
+
+        return $volunteering;
     }
 }
