7231303f99001cbd1049129995166726426cb512
Adrien Roches <adrien@roc-it.tech>
2026-03-12 11:14:08 +0100

    [sf-advanced] Use search interface in list conference controller

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Conference;
 use App\Form\ConferenceType;
-use App\Search\Conference\DatabaseConferenceSearch;
+use App\Search\Conference\ConferenceSearchInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -44,7 +44,7 @@ class ConferenceController extends AbstractController
         name: 'app_conference_list',
         methods: ['GET'],
     )]
-    public function list(Request $request, DatabaseConferenceSearch $conferenceSearch): Response
+    public function list(Request $request, ConferenceSearchInterface $conferenceSearch): Response
     {
         $conferences = $conferenceSearch->searchByName($request->query->getString('name'));
 
